da283ce28227747f95114d67833325ed6d81b0cb
mpmcauliffe <mcauliffe99@gmail.com>
2021-09-26 22:47:19 -0600

    test deploy for gatsby cloud

---
+++ src/components/Layout.js
@@ -0,0 +1,37 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+ import * as React from 'react'
+ import { Link } from 'gatsby'
+ import { container, heading, navLinks, navLinkItem, navLinkText, } from './layout.module.css'
+
+ 
+ export const Layout = ({ pageTitle, children }) => {
+    return (
+        <div className={container}>
+            <title>{pageTitle}</title>
+            <nav>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}>
+                        <Link to="/" className={navLinkText}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/about" className={navLinkText}>
+                            About
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <main>
+                <h1 className={heading}>{pageTitle}</h1>
+                {children}
+            </main>
+        </div>
+    )
+ }
