a416e401f9526ba9754f5b7c05cf84ee1adf1248
mpmcauliffe <mcauliffe99@gmail.com>
2021-09-27 23:41:29 -0600

    add basic metadata query and create /blog route

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,14 +6,26 @@
  */
 
  import * as React from 'react'
- import { Link } from 'gatsby'
- import { container, heading, navLinks, navLinkItem, navLinkText, } from './layout.module.css'
+ import { Link, useStaticQuery, graphql,  } from 'gatsby'
+ import { container, heading, navLinks, navLinkItem, navLinkText, siteTitle, } from './layout.module.css'
 
  
  export const Layout = ({ pageTitle, children }) => {
+    const data = useStaticQuery(graphql`
+        query {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+        }
+    `)
+
     return (
         <div className={container}>
-            <title>{pageTitle}</title>
+            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+
             <nav>
                 <ul className={navLinks}>
                     <li className={navLinkItem}>
@@ -24,6 +36,11 @@
                     <li className={navLinkItem}>
                         <Link to="/about" className={navLinkText}>
                             About
+                        </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/blog" className={navLinkText}>
+                            Blog
                         </Link>
                     </li>
                 </ul>
---
+++ src/pages/blog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Layout, } from '../components'
+
+
+const blog = () => {
+    return (
+        <Layout pageTitle="My Blog Posts">
+            <p>My cool posts will go in here</p>
+        </Layout>
+    )
+}
+
+
+export default blog
